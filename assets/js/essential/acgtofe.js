a098c67686e8df7177b6d593a19cc5f78e5b80e2
kenanpengyou <kenanpengyou@yeah.net>
2013-04-14 16:19:57 +0800

    new post lovelive-ed

---
+++ assets/js/essential/acgtofe.js
@@ -0,0 +1,74 @@
+/*
+* acgtofe.js
+* Based on jquery-1.9.1
+* By Liang Zhu(kenanpengyou@yeah.net)
+* Date 2013-03-08
+*/
+
+ (function($) {
+
+    //return to top
+    var returnToTop = function() {
+        var handleId = "returnTop",
+            handleShowOffset = 200,
+            handleShowDuration = 300,
+            handlePos = {
+                right: 0,
+                bottom: 50
+            },
+            returnDuration = 100,
+            scrollPos = 0,
+            scrollPosLast = 0,
+            windowResize = false,
+            cssFixSupport = !window.ActiveXObject || window.XMLHttpRequest;
+
+        var handleElement = $("#" + handleId),
+            posString = cssFixSupport ? "fixed" : "absolute";
+
+        var handleControl = function() {
+            var scrollPos = $(document).scrollTop();
+            if (scrollPosLast <= handleShowOffset && scrollPos > handleShowOffset) {
+                handleElement.fadeIn(handleShowDuration);
+            } else if (scrollPosLast > handleShowOffset && scrollPos <= handleShowOffset) {
+                handleElement.fadeOut(handleShowDuration);
+            }
+
+            if (!cssFixSupport) {
+                handleElement.css("top", scrollPos + $(window).height() - handlePos.bottom);
+            } else if (windowResize) {
+                windowResize = false;
+                handleElement.css("top", $(window).height() - handlePos.bottom);
+            }
+
+            //update value
+            scrollPosLast = scrollPos;
+        }
+
+        handleElement.css({
+            "position": posString,
+            "right": handlePos.right,
+            "top": $(window).height() - handlePos.bottom
+        });
+
+        //it's better to set the handleElement "display:none" in css previously if using static HTML
+        handleElement.hide();
+
+        handleElement.on("click", function(event) {
+            $("html, body").animate({
+                scrollTop: 0
+            }, handleShowDuration);
+        })
+        $(window).on("scroll", function() {
+            handleControl();
+        }).on("resize", function() {
+            windowResize = true;
+            handleControl();
+        })
+    }
+
+    //functions which run after page ready
+    $(function() {
+        returnToTop();
+    });
+
+})(jQuery);
